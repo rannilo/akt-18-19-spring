b341fefff0c6cb67acf234f05c5cff70321f3907
Vesal Vojdani <vesal.vojdani@gmail.com>
2019-04-04 12:58:19 +0300

    Lisatöö update.

--- src/main/java/week6/stack/Evaluator.java
+++ src/main/java/week6/stack/Evaluator.java
@@ -12,28 +12,35 @@ public class Evaluator {
 
     public static List<String> tokenize(String input) {
         List<String> tokens = new ArrayList<>();
-        String currentToken = null;
+        StringBuilder currentToken = new StringBuilder();         // Tühi sõne tähistab seisundit ￢TOK
         for (char c : input.toCharArray()) {
             if (c == '#') break;
-            if (c == '+' || c == '-' || c == '=' || c == '(' || c == ')' || c == ' ') {
-                if (currentToken != null) {
-                    tokens.add(currentToken);
-                    currentToken = null;
+            if (c == '+' || c == '-' || c == '=' || c == ' ' || c == '(' || c == ')') {   // lisame siia sulud.
+                if (currentToken.length() != 0) {
+                    tokens.add(currentToken.toString());          // push
+                    currentToken = new StringBuilder();
                 }
-                if (c != ' ') tokens.add(Character.toString(c));
+                if (c != ' ') tokens.add(Character.toString(c));  // echo (v.a. tühikud)
             } else {
-                if (currentToken != null) currentToken += c;
-                else currentToken = Character.toString(c);
+                currentToken.append(c);                           // acc
             }
         }
-        if (currentToken != null) tokens.add(currentToken);
+        // Ja lõpuks EOF puhul ka push seisundis TOK:
+        if (currentToken.length() != 0) tokens.add(currentToken.toString());
         return tokens;
     }
 
     public static int compute(List<String> tokens, Map<String, Integer> env) {
+        return compute(tokens, env, true);
+    }
+
+    public static int compute(List<String> tokens, Map<String, Integer> env, boolean allowPartial) {
         EvalMachine machine = new EvalMachine(env);
         int result = 0;
         for (String token : tokens) result = machine.compute(token);
+        // Peab suutma tagastama, kas oli väärtus, et viimased AktPdaTest testid läbi läheks.
+        // Moodle'i testide läbimiseks ei ole seda vaja.
+        // if (!allowPartial && !machine.wasValue()) throw new RuntimeException("Malformed expression!");
         return result;
     }
 
@@ -48,11 +55,25 @@ public class Evaluator {
         Map<String, Integer> env = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
             for (String line; (line = br.readLine()) != null; ) {
+                if (line.length() > 0 && line.charAt(0) == ' ') throw new RuntimeException("Begins with space!");
                 List<String> tokens = tokenize(line);
                 if (tokens.size() < 1) continue;
-                if (tokens.get(0).equals("print")) System.out.println(compute(tokens.subList(1, tokens.size()), env));
-                if (tokens.get(1).equals("=")) env.put(tokens.get(0), compute(tokens.subList(2, tokens.size()), env));
+                String firstToken = tokens.get(0);
+                if (firstToken.equals("print")) {
+                    System.out.println(compute(tokens.subList(1, tokens.size()), env, false));
+                } else if (tokens.get(1).equals("=")) {
+                    if (firstToken.length() > 1) throw new RuntimeException("Too long var!");
+                    if (!isLatinLetter(firstToken.charAt(0))) throw new RuntimeException("Nonlatin var name.");
+                    env.put(firstToken, compute(tokens.subList(2, tokens.size()), env, false));
+                } else {
+                    throw new RuntimeException("Unexpected operation!");
+                }
             }
         }
     }
+
+    private static boolean isLatinLetter(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+    }
+
 }
---
+++ src/test/java/week6/stack/AktkPdaTest.java
@@ -0,0 +1,10 @@
+package week6.stack;
+
+import week1.AKTKiTestV2;
+
+public class AktkPdaTest extends AKTKiTestV2 {
+	@Override
+	protected String getClassNameToTest() {
+		return "week6.stack.Evaluator";
+	}
+}
